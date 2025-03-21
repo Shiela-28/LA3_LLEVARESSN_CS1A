f8f60aa9ca35429bcb9659422756f5a88e2ccae7
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-22 05:19:14 +0800

    Add files via upload

---
+++ gradeAssignment_v2.js
@@ -0,0 +1,20 @@
+let score = prompt("Enter your score:");
+
+let grade;
+let remarks;
+
+grade = score >= 97 ? "1.00" : score >= 94 ? "1.25" : score >= 91 ? "1.50" : score >= 88 ? "1.75" : score >= 85 ? "2.00" : score >= 82 ? "2.25" : score >= 79 ? "2.50" : score >= 76 ? "2.75" : score === 75 ? "3.00" : score >= 72 ? "4.00" : "5.00";
+
+if (score >= 90) {
+  remarks = "HIGH PASS, Candidate for Cum Laude";
+} else if (score >= 80) {
+  remarks = "AVERAGE PASS";
+} else if (score >= 75) {
+  remarks = "LOW PASS";
+} else {
+  remarks = "FAILED, Needs Improvement";
+}
+
+
+console.log("Your equivalent Grade is \"" + grade + "\"");
+console.log("Final Remarks: " + remarks);
