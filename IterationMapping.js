d476869ec41217c45f18ff122818c3e77cec604e
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-22 10:34:25 +0800

    Add files via upload

--- IterationMapping.js
+++ IterationMapping.js
@@ -1,9 +1,9 @@
-let maxLimit = prompt("Enter the limit of the double loop:");
-let addedValue = 0;
+let maxLimit = prompt("Enter the limit of the double loop:"); // Get limit input
+let addedValue = 0; //initialize sum
 
-for (let i = 0; i < maxLimit; i++) {
-  for (let j = 0; j < maxLimit; j++) {
-    addedValue = i + j;
-    console.log("[" + i + "] [" + j + "] Added value is " + addedValue);
+for (let i = 0; i < maxLimit; i++) { //outer loop
+  for (let j = 0; j < maxLimit; j++) { //inner loop
+    addedValue = i + j; // Calculate sum
+    console.log("[" + i + "] [" + j + "] Added value is " + addedValue); //Display results
   }
 }
--- gradeAssignment_v2.js
+++ gradeAssignment_v2.js
@@ -1,10 +1,12 @@
-let score = prompt("Enter your score:");
+let score = prompt("Enter your score:"); // Get score input
 
-let grade;
-let remarks;
+let grade; // Declare grade variable 
+let remarks; // Declare grade remarks
 
+//Assign grade using nested ternary operator
 grade = score >= 97 ? "1.00" : score >= 94 ? "1.25" : score >= 91 ? "1.50" : score >= 88 ? "1.75" : score >= 85 ? "2.00" : score >= 82 ? "2.25" : score >= 79 ? "2.50" : score >= 76 ? "2.75" : score === 75 ? "3.00" : score >= 72 ? "4.00" : "5.00";
 
+//Assign remarks based on score
 if (score >= 90) {
   remarks = "HIGH PASS, Candidate for Cum Laude";
 } else if (score >= 80) {
@@ -15,6 +17,6 @@ if (score >= 90) {
   remarks = "FAILED, Needs Improvement";
 }
 
-
+// output results
 console.log("Your equivalent Grade is \"" + grade + "\"");
 console.log("Final Remarks: " + remarks);
